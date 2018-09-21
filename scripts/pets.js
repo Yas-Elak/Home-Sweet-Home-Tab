7053060f8752f620990793a5fc3885a92f5caff2
Yassine El Alaoui Lakhnati <elalaoui87@gmail.com>
2018-09-21 11:02:00 +0200

    Blink eyes

--- scripts/pets.js
+++ scripts/pets.js
@@ -129,6 +129,8 @@ window.setInterval(function(){
     } else {
       $(".pet-eyes-img").attr("src", "images/cats/standing-cat-eyes.svg");
     }
-    blinkeyes = Math.floor((Math.random() * (5000 - 2000) )) + 2000;
+    blinkeyes = Math.floor((Math.random() * (10000 - 3250) )) + 3250;
   }}, blinkeyes);
+
+
 });
