95fd9dd7ad6b842557100ffe88e3e0bf77dae458
Yassine El Alaoui Lakhnati <elalaoui87@gmail.com>
2018-09-25 00:57:51 +0200

    Bug correction

--- scripts/background.js
+++ scripts/background.js
@@ -11,53 +11,5 @@ document.body.style.background = "#D3D3D3 url(images/backgrounds/" + backgroundN
 
 //TODO Add a way to choose the backgrouns and apply it for every session
 
-//testing-----------------------------------------------------------------------------------------------------------------------------
-/*var tabToUrl = {};
-
-chrome.windows.getAll({populate:true},function(windows){
-  windows.forEach(function(window){
-    window.tabs.forEach(function(tab){
-      //here  get all the tab open when the newtab and put it in a array
-      console.log(tab.url);
-      tabToUrl[tabId] = tab.url;
-
-      //collect all of the urls here, I will just log them instead
-    });
-  });
-});
-/*
-chrome.tabs.query({currentWindow: true}, function(tabs) {
-  myArray = myArray.filter( function( el ) {
-      return !toRemove.includes( el );
-  });
-    });
-
-chrome.tabs.query({currentWindow: true}, function(tab){
-    for (var i = 0; i < tab.length; i++) {
-  //  console.log(tab.url);
-    };
-  });
-  */
-/*
-  let recentlyClosed = [];
-  chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-      // Note: this event is fired twice:
-      // Once with `changeInfo.status` = "loading" and another time with "complete"
-      //then to each update I add the url to the array
-      tabToUrl[tabId] = tab.url;
-  });
-  chrome.tabs.onRemoved.addListener(function(tabId, removeInfo) {
-      console.log(tabToUrl[tabId]);
-      recentlyClosed.push(tabToUrl[tabId]);
-      if (recentlyClosed.length > 7){
-        recentlyClosed.shift();
-      }
-      //then if a tab s close and it's not a new tab or undifiner, it goes in my list
-      console.log(recentlyClosed)
-      chrome.storage.local.set({ "tabhandler": recentlyClosed });
-  });
-
-  */
-
 
 });
